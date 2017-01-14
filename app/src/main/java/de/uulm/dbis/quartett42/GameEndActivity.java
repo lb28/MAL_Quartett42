11817eecf6dd96d5664228dd2d5449c8955fb9c7
Tim Stenzel <tim.stenzel@uni-ulm.de>
2017-01-14 20:46:52 +0100

    a

--- app/src/main/java/de/uulm/dbis/quartett42/GameEndActivity.java
+++ app/src/main/java/de/uulm/dbis/quartett42/GameEndActivity.java
@@ -1,5 +1,6 @@
 package de.uulm.dbis.quartett42;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ public class GameEndActivity extends AppCompatActivity {
 
     SharedPreferences sharedPref;
     SharedPreferences.Editor editor;
+    Intent intent;
 
     //Elemente:
 
