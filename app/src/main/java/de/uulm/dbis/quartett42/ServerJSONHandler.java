427235b391082ecbbe532913ee3a9648f8555379
Fabian Fischbach <fabian.fischbach@uni-ulm.de>
2017-01-21 16:39:01 +0100

    getDeck(int deckID) gemacht aber nicht getestet

--- app/src/main/java/de/uulm/dbis/quartett42/ServerJSONHandler.java
+++ app/src/main/java/de/uulm/dbis/quartett42/ServerJSONHandler.java
@@ -11,9 +11,14 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 
+import de.uulm.dbis.quartett42.data.Card;
 import de.uulm.dbis.quartett42.data.Deck;
 import de.uulm.dbis.quartett42.data.ImageCard;
+import de.uulm.dbis.quartett42.data.Property;
+
+import static de.uulm.dbis.quartett42.data.Deck.SRC_MODE_SERVER;
 
 /**
  * a JSON Handler for the Server
@@ -21,6 +26,7 @@ import de.uulm.dbis.quartett42.data.ImageCard;
  */
 public class ServerJSONHandler {
     public static final String URL_DECKS_OVERVIEW = "http://quartett.af-mba.dbis.info/decks/";
+    private static final String URL_AUTHORIZATION = "Basic YWRtaW46ZGIxJGFkbWlu";
 
 
     private Context context;
@@ -46,7 +52,7 @@ public class ServerJSONHandler {
                 String imageUri = onlineDeck.getString("image");
 
                 ImageCard newImage = new ImageCard(imageUri, deckDescription);
-                Deck newDeck = new Deck(deckName, newImage, null, null, Deck.SRC_MODE_SERVER);
+                Deck newDeck = new Deck(deckName, newImage, null, null, SRC_MODE_SERVER);
                 newDeck.setID(deckID);
                 onlineDecks.add(newDeck);
             }
@@ -63,78 +69,107 @@ public class ServerJSONHandler {
      * Loads a single deck. The location where this method looks for the deck depends on
      * the jsonMode set in the constructor (assets/internal/both).
      *
+     * The image of the deck and the cards is still the online URL
+     * the picture has to be downloaded and saved when parsing the data object to JSON
      *
-     * @param chosenDeckName the name of the deck
+     * @param chosenDeckID the if of the deck which wants to be downloaded
      * @return the deck with the specified name, or null if no deck was found
      */
-/*
-    public Deck getDeck(String chosenDeckName){
+
+    public Deck getDeck(int chosenDeckID){
+        Deck resultDeck = null;
         try {
-            JSONObject jsonObj = readJSONFromFile(jsonMode);
-            JSONArray decks = jsonObj.getJSONArray("decks");
-            //Nach passendem Deck im JSON-Array suchen:
-            for (int i = 0; i < decks.length(); i++) {
-                JSONObject tmpDeck = decks.getJSONObject(i);
-                String deckName = tmpDeck.getString("name");
-                if(deckName.equals(chosenDeckName)){
-                    //Alle Daten fuer das Deck auslesen:
-                    String deckDescription = tmpDeck.getString("description");
-                    String deckImageUri = tmpDeck.getString("image");
-                    ImageCard deckImage = new ImageCard(deckImageUri, deckDescription);
-                    //Properties auslesen:
-                    ArrayList<Property> propertyList = new ArrayList<Property>();
-                    JSONArray properties = tmpDeck.getJSONArray("properties");
-                    for(int p = 0; p < properties.length(); p++){
-                        JSONObject tmpProperty = properties.getJSONObject(p);
-                        String pName = tmpProperty.getString("name");
-                        String pUnit = tmpProperty.getString("unit");
-                        boolean pMaxwinner = tmpProperty.getBoolean("maxwinner");
-                        Property property = new Property(pName, pUnit, pMaxwinner);
-                        propertyList.add(property);
+            //1. Load Deck information:
+            String deckURL = URL_DECKS_OVERVIEW+chosenDeckID+"/";
+            String jsonStringDeck = loadOnlineData(new URL(deckURL));
+
+            JSONObject onlineDeck = new JSONObject(jsonStringDeck);
+            String deckName = onlineDeck.getString("name");
+            String deckDescription = onlineDeck.getString("description");
+            String deckImageURL = onlineDeck.getString("image");
+            ImageCard deckImage = new ImageCard(deckImageURL, deckDescription);
+
+            //2. Load List of all cards:
+            String deckCardsURL = deckURL+"cards/";
+            String jsonStringCards = loadOnlineData(new URL(deckCardsURL));
+
+            ArrayList<Property> propertyArrayList = new ArrayList<Property>();
+            ArrayList<Card> cardArrayList = new ArrayList<Card>();
+
+            JSONArray onlineCardsArray = new JSONArray(jsonStringCards);
+            for(int i = 0; i < onlineCardsArray.length(); i++){
+                JSONObject tmpIDCard = onlineCardsArray.getJSONObject(i);
+                int onlineCardID = tmpIDCard.getInt("id");
+                //Name kann weggelassen werden... kommt nochmal
+
+
+                //3. Load Data for single Card:
+                String singleCardURL = deckCardsURL+onlineCardID+"/";
+                String jsonStringSingleCard = loadOnlineData(new URL(singleCardURL));
+
+                JSONObject onlineSingleCard = new JSONObject(jsonStringSingleCard);
+                String cardName = onlineSingleCard.getString("name");
+
+                HashMap<String, Double> tmpAttributeHashmap = new HashMap<String, Double>();
+
+
+                //4. Load Properties
+                String propertiesURL = singleCardURL+"attributes/";
+                String jsonStringProperties = loadOnlineData(new URL(propertiesURL));
+
+                JSONArray attributeArrayList = new JSONArray(jsonStringProperties);
+                if(i == 0){ //do this only once
+                    for(int p = 0; p < attributeArrayList.length(); p++){
+                        JSONObject onlineProperty = attributeArrayList.getJSONObject(p);
+                        String propertyName = onlineProperty.getString("name");
+                        String propertyUnit = onlineProperty.getString("unit");
+                        boolean maxWinner = onlineProperty.getString("what_wins").equals("higher_wins");
+
+                        Property tmpProperty = new Property(propertyName, propertyUnit, maxWinner);
+                        propertyArrayList.add(tmpProperty);
                     }
-                    //Alle Karten auslesen:
-                    ArrayList<Card> cardList = new ArrayList<Card>();
-                    JSONArray cards = tmpDeck.getJSONArray("cards");
-                    for(int c = 0; c < cards.length(); c++){
-                        JSONObject tmpCard = cards.getJSONObject(c);
-                        String cName = tmpCard.getString("name");
-                        int cId = tmpCard.getInt("id");
-                        //Bilder durchlaufen:
-                        ArrayList<ImageCard> imageList = new ArrayList<ImageCard>();
-                        JSONArray images = tmpCard.getJSONArray("images");
-                        for(int j = 0; j < images.length(); j++){
-                            JSONObject tmpImage = images.getJSONObject(j);
-                            String uri = tmpImage.getString("URI");
-                            String iDescription = tmpImage.getString("description");
-                            ImageCard cardImage = new ImageCard(uri, iDescription);
-                            imageList.add(cardImage);
-                        }
-                        //Values durchlaufen:
-                        HashMap<String, Double> attributeMap = new HashMap<String, Double>();
-                        JSONObject values = tmpCard.getJSONObject("values");
-                        for(Property px : propertyList){
-                            //System.out.println(px.getName()+": "+values.getDouble(px.getName()));
-                            attributeMap.put(px.getName(), values.getDouble(px.getName()));
-                        }
-                        //Karte erstellen
-                        Card newCard = new Card(cName, cId, imageList, attributeMap);
-                        cardList.add(newCard);
-                    }
-                    //return the deck
-                    return new Deck(chosenDeckName, deckImage, propertyList, cardList, Deck.SRC_MODE_SERVER);
                 }
+
+
+                //5. Load Attributes for every Card
+                for(int p = 0; p < attributeArrayList.length(); p++){
+                    JSONObject onlineProperty = attributeArrayList.getJSONObject(p);
+                    String propertyName = onlineProperty.getString("name");
+                    Double propertyValue = onlineProperty.getDouble("value");
+                    tmpAttributeHashmap.put(propertyName, propertyValue);
+                }
+
+                //6. Load Images and Descriptions
+                String cardImagesURL = singleCardURL+"images/";
+                String jsonCardImages = loadOnlineData(new URL(cardImagesURL));
+
+                ArrayList<ImageCard> tmpImageList = new ArrayList<ImageCard>();
+                JSONArray cardImagesArray = new JSONArray(jsonCardImages);
+                for(int l = 0; l < cardImagesArray.length(); l++){
+                    JSONObject onlineCardImage = cardImagesArray.getJSONObject(l);
+                    String cardImageURL = onlineCardImage.getString("image");
+                    String cardImageDescription = onlineCardImage.getString("description");
+
+                    ImageCard tmpCardImage = new ImageCard(cardImageURL, cardImageDescription);
+                    tmpImageList.add(tmpCardImage);
+                }
+
+                //7. Built Card together and add it to the ArrayList
+                Card tmpCard = new Card(cardName, i, tmpImageList, tmpAttributeHashmap);
+                cardArrayList.add(tmpCard);
+
             }
+
+            //8. Built deck together:
+            resultDeck = new Deck(deckName, deckImage, propertyArrayList, cardArrayList, SRC_MODE_SERVER);
 
         } catch (Exception e){
             e.printStackTrace();
         }
 
         // no deck was found
-        return null;
+        return resultDeck;
     }
-
-
-*/
 
 
 
@@ -153,7 +188,7 @@ public class ServerJSONHandler {
         HttpURLConnection urlConnection = null;
         try {
             urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestProperty("Authorization", "Basic YWRtaW46ZGIxJGFkbWlu");
+            urlConnection.setRequestProperty("Authorization", URL_AUTHORIZATION);
             InputStream in = new BufferedInputStream(urlConnection.getInputStream());
             jsonString = "";
             byte[] buffer = new byte[1024];
