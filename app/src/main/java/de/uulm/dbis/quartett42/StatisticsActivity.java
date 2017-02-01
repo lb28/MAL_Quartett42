8c08c890b2767d1d6fedae64f355eb3c50eb8b9c
Luis Nicolas Beaucamp <luis.beaucamp@uni-ulm.de>
2017-02-01 12:37:37 +0100

    pieCharts fix

--- app/src/main/java/de/uulm/dbis/quartett42/StatisticsActivity.java
+++ app/src/main/java/de/uulm/dbis/quartett42/StatisticsActivity.java
@@ -1,31 +1,20 @@
 package de.uulm.dbis.quartett42;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.net.Uri;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.ComponentBase;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -154,7 +143,7 @@ public class StatisticsActivity extends AppCompatActivity {
 
         setDataForPieChart();
 
-        //Uri uri = Uri.parse(context.getFilesDir() + "/Spielegesamt.jpg");
+        //Uri uri = Uri.parse(getFilesDir() + "/Spielegesamt.jpg");
         //imageView1.setImageURI(uri);
 
         pieChart.invalidate();
@@ -165,7 +154,7 @@ public class StatisticsActivity extends AppCompatActivity {
 
     public void setDataForPieChart(){
 
-        context = this.getApplicationContext();
+        //context = this.getApplicationContext();
 
         int spieleGesamt = sharedPref.getInt("spieleGesamt", 0);
         int spieleGesamtGewonnen = sharedPref.getInt("spieleGesamtGewonnen", 0);
@@ -202,8 +191,8 @@ public class StatisticsActivity extends AppCompatActivity {
         //----------------------------
 
         ArrayList<PieEntry> yVals2 = new ArrayList<>();
-        yVals.add(new PieEntry(normalespielegewonnen, xValues[0]));
-        yVals.add(new PieEntry(normalespieleverloren, xValues[1]));
+        yVals2.add(new PieEntry(normalespielegewonnen, xValues[0]));
+        yVals2.add(new PieEntry(normalespieleverloren, xValues[1]));
 
         //create pieDataSet
         pieDataSet2 =  new PieDataSet(yVals2, "");
