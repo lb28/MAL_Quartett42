fac440db1f1ae5c06d770b426beaf6b04f775ff5
Tim Stenzel <tim.stenzel@uni-ulm.de>
2017-02-01 17:35:31 +0100

    coole pie charts fertig

--- app/src/main/java/de/uulm/dbis/quartett42/StatisticsActivity.java
+++ app/src/main/java/de/uulm/dbis/quartett42/StatisticsActivity.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
@@ -34,13 +35,11 @@ public class StatisticsActivity extends AppCompatActivity {
     TextView expertSpiele;
     TextView expertSpielegewonnen;
 
-    ImageView imageView1;
+    PieChart pieChart, pieChart2, pieChart3, pieChart4;
+    PieDataSet pieDataSet, pieDataSet2, pieDataSet3, pieDataSet4, pieDataSet5;
+    PieData pieData, pieData2, pieData3, pieData4, pieData5;
 
-    PieChart pieChart, pieChart2;
-    PieDataSet pieDataSet, pieDataSet2;
-    PieData pieData, pieData2;
-
-    private final String[] xValues = {"Gewonnen", "Verloren"};
+    private final String[] xValues = {"Wins", "Losses"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -138,35 +137,40 @@ public class StatisticsActivity extends AppCompatActivity {
         //pie chart
         pieChart = (PieChart) findViewById(R.id.pieChart);
         pieChart2 = (PieChart) findViewById(R.id.pieChart2);
-
-        //imageView1 = (ImageView) findViewById(R.id.pieChart1);
+        pieChart3 = (PieChart) findViewById(R.id.pieChart3);
+        pieChart4 = (PieChart) findViewById(R.id.pieChart4);
 
         setDataForPieChart();
 
-        //Uri uri = Uri.parse(getFilesDir() + "/Spielegesamt.jpg");
-        //imageView1.setImageURI(uri);
-
         pieChart.invalidate();
         pieChart2.invalidate();
-
-
+        pieChart3.invalidate();
+        pieChart4.invalidate();
     }
 
     public void setDataForPieChart(){
 
-        //context = this.getApplicationContext();
-
         int spieleGesamt = sharedPref.getInt("spieleGesamt", 0);
         int spieleGesamtGewonnen = sharedPref.getInt("spieleGesamtGewonnen", 0);
-        int spieleGesamtVerloren = spieleGesamt-spieleGesamtGewonnen;
+        int spieleGesamtVerloren = spieleGesamt - spieleGesamtGewonnen;
 
         int normalespiele = sharedPref.getInt("normaleSpiele", 0);
         int normalespielegewonnen = sharedPref.getInt("normaleSpieleGewonnen", 0);
-        int normalespieleverloren = normalespiele-normalespielegewonnen;
+        int normalespieleverloren = normalespiele - normalespielegewonnen;
+
+        int insanespiele = sharedPref.getInt("insaneSpiele", 0);
+        int insanespielegewonnen = sharedPref.getInt("insaneSpieleGewonnen", 0);
+        int insanespieleverloren = insanespiele - insanespielegewonnen;
+
+        int expertspiele = sharedPref.getInt("expertSpiele", 0);
+        int expertspielegewonnen = sharedPref.getInt("expertSpieleGewonnen", 0);
+        int expertspieleverloren = expertspiele - expertspielegewonnen;
 
         ArrayList<Integer> colors = new ArrayList<>();
-        colors.add(Color.GREEN);
-        colors.add(Color.RED);
+        colors.add(Color.rgb(20,205,50));
+        colors.add(Color.rgb(255, 20, 0));
+
+        //----------------- spiele gesamt -------------------------
 
         ArrayList<PieEntry> yVals = new ArrayList<>();
         yVals.add(new PieEntry(spieleGesamtGewonnen, xValues[0]));
@@ -178,6 +182,7 @@ public class StatisticsActivity extends AppCompatActivity {
         pieDataSet.setSelectionShift(5);
         pieDataSet.setColors(colors);
         pieDataSet.setValueTextSize(12f);
+        pieDataSet.setValueTextColor(Color.BLACK);
 
         //create pie data object and set xValues and yValues and set it to the pie chart
         pieData = new PieData(pieDataSet);
@@ -186,9 +191,12 @@ public class StatisticsActivity extends AppCompatActivity {
         pieChart.setDescription(null);
         pieChart.setCenterText("Spiele gesamt: " + spieleGesamt);
 
-        //pieChart.saveToPath("Spielegesamt", context.getFilesDir() + "/");
+        if (spieleGesamt == 0) {
+            Legend l = pieChart.getLegend();
+            l.setEnabled(false);
+        }
 
-        //----------------------------
+        //----------------- normale spiele -------------------------
 
         ArrayList<PieEntry> yVals2 = new ArrayList<>();
         yVals2.add(new PieEntry(normalespielegewonnen, xValues[0]));
@@ -209,10 +217,63 @@ public class StatisticsActivity extends AppCompatActivity {
         pieChart2.setDescription(null);
         pieChart2.setCenterText("Normale Spiele: " + normalespiele);
 
+        if (normalespiele == 0) {
+            Legend l = pieChart2.getLegend();
+            l.setEnabled(false);
+        }
+
+        //----------------- insane spiele -------------------------
+
+        ArrayList<PieEntry> yVals3 = new ArrayList<>();
+        yVals3.add(new PieEntry(insanespielegewonnen, xValues[0]));
+        yVals3.add(new PieEntry(insanespieleverloren, xValues[1]));
+
+        //create pieDataSet
+        pieDataSet3 =  new PieDataSet(yVals3, "");
+        pieDataSet3.setSliceSpace(3);
+        pieDataSet3.setSelectionShift(5);
+        pieDataSet3.setColors(colors);
+        pieDataSet3.setValueTextSize(12f);
+
+
+        //create pie data object and set xValues and yValues and set it to the pie chart
+        pieData3 = new PieData(pieDataSet3);
+
+        pieChart3.setData(pieData3);
+        pieChart3.setDescription(null);
+        pieChart3.setCenterText("Insane Spiele: " + insanespiele);
+
+        if (insanespiele == 0) {
+            Legend l = pieChart3.getLegend();
+            l.setEnabled(false);
+        }
+
+        //----------------- expert spiele -------------------------
+
+        ArrayList<PieEntry> yVals4 = new ArrayList<>();
+        yVals4.add(new PieEntry(expertspielegewonnen, xValues[0]));
+        yVals4.add(new PieEntry(expertspieleverloren, xValues[1]));
+
+        //create pieDataSet
+        pieDataSet4 =  new PieDataSet(yVals4, "");
+        pieDataSet4.setSliceSpace(3);
+        pieDataSet4.setSelectionShift(5);
+        pieDataSet4.setColors(colors);
+        pieDataSet4.setValueTextSize(12f);
 
 
 
+        //create pie data object and set xValues and yValues and set it to the pie chart
+        pieData4 = new PieData(pieDataSet4);
 
+        pieChart4.setData(pieData4);
+        pieChart4.setDescription(null);
+        pieChart4.setCenterText("Expert Spiele: " + expertspiele);
+
+        if (expertspiele == 0) {
+            Legend l = pieChart4.getLegend();
+            l.setEnabled(false);
+        }
 
     }
 
@@ -238,6 +299,7 @@ public class StatisticsActivity extends AppCompatActivity {
 
         Toast.makeText(getApplicationContext(), "Statistik zurückgesetzt", Toast.LENGTH_SHORT).show();
 
+        /*
         //setze alle Felder auf die Defaults
         Spielegesamt.setText("" + 0);
         Spielegesamtgewonnen.setText("" + 0 + " (" + 0 + " %)");
@@ -250,6 +312,54 @@ public class StatisticsActivity extends AppCompatActivity {
 
         expertSpiele.setText("" + 0);
         expertSpielegewonnen.setText("" + 0 + " (" + 0 + " %)");
+        */
+
+        ArrayList<PieEntry> yVals5 = new ArrayList<>();
+        yVals5.add(new PieEntry(0, xValues[0]));
+        yVals5.add(new PieEntry(0, xValues[1]));
+
+        //create pieDataSet
+        pieDataSet5 =  new PieDataSet(yVals5, "");
+        pieDataSet5.setSliceSpace(3);
+        pieDataSet5.setSelectionShift(5);
+        //pieDataSet5.setColors(colors);
+        pieDataSet5.setValueTextSize(12f);
+
+
+        //create pie data object and set xValues and yValues and set it to the pie chart
+        pieData5 = new PieData(pieDataSet5);
+
+        pieChart.setData(pieData5);
+        pieChart.setDescription(null);
+        pieChart.setCenterText("Spiele gesamt: " + 0);
+        pieChart.invalidate();
+
+        pieChart2.setData(pieData5);
+        pieChart2.setDescription(null);
+        pieChart2.setCenterText("Normale Spiele: " + 0);
+        pieChart2.invalidate();
+
+        pieChart3.setData(pieData5);
+        pieChart3.setDescription(null);
+        pieChart3.setCenterText("Insane Spiele: " + 0);
+        pieChart3.invalidate();
+
+        pieChart4.setData(pieData5);
+        pieChart4.setDescription(null);
+        pieChart4.setCenterText("Expert Spiele: " + 0);
+        pieChart4.invalidate();
+
+        Legend l = pieChart.getLegend();
+        l.setEnabled(false);
+
+        Legend l2 = pieChart2.getLegend();
+        l.setEnabled(false);
+
+        Legend l3 = pieChart3.getLegend();
+        l.setEnabled(false);
+
+        Legend l4 = pieChart4.getLegend();
+        l.setEnabled(false);
 
         finish();
 
